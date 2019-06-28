7a05831baad4efe6fbfdb1daf5c8a80ed7a3b26c
Cristian <cristian.tejadan@gmail.com>
2019-06-28 16:38:57 -0400

    added redis in coordinates route

--- src/api/coordinates/routes.js
+++ src/api/coordinates/routes.js
@@ -1,9 +1,22 @@
 const router = require('koa-router')()
 const controller = require('./controller')
-
+const redis = require("redis")
+const client = redis.createClient()
+const timeToLive = 60
 router.get('/:city', async ctx => {
   const { city } = ctx.params
-  ctx.body = await controller.byCity(city)
+  const storedInRedis = client.getAsync(city)
+
+  if (storedInRedis) {
+    console.log("saved in redis :D")
+    return "stored in redis" + storedInRedis
+  } else {
+    const body = await controller.byCity(city)
+
+    client.setexAsync(city, timeToLive, JSON.stringify(body))
+
+    ctx.body = body
+  }
 })
 
 module.exports = router.routes()

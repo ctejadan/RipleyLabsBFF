6a67b9a5bc5effe0fc7f0475fa498c0255f74c7c
Cristian <cristian.tejadan@gmail.com>
2019-06-28 17:04:05 -0400

    debug redis heroku

--- src/api/coordinates/routes.js
+++ src/api/coordinates/routes.js
@@ -9,7 +9,7 @@ const client = redis.createClient({
 const timeToLive = 60
 router.get('/:city', async ctx => {
   const { city } = ctx.params
-  const storedInRedis = await client.get(city).then(result => result).catch(e => console.log(e)
+  const storedInRedis = await client.get(city).then(result => result).catch(e => console.log(e))
 
   console.log(storedInRedis)
 

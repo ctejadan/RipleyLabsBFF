7e13022ca9548e06352d216806c63a39b82e3843
Cristian <cristian.tejadan@gmail.com>
2019-06-28 16:48:08 -0400

    added heroku redis data

--- src/api/coordinates/routes.js
+++ src/api/coordinates/routes.js
@@ -1,7 +1,11 @@
 const router = require('koa-router')()
 const controller = require('./controller')
 const redis = require("redis")
-const client = redis.createClient()
+const client = redis.createClient({
+  host: 'ec2-52-206-114-45.compute-1.amazonaws.com',
+  port: 10669,
+  password: 'p29f2f880bb71614588c5a88b9bf27bba0c0438f550d0928eb9f079b2d4e71579'
+})
 const timeToLive = 60
 router.get('/:city', async ctx => {
   const { city } = ctx.params
